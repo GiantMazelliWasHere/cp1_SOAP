f54d7dbe9d804effee6c90fcf6008ef08677f60a
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2025-09-02 11:19:22 -0300

    Update do projeto

---
+++ Winery/Consumer/WineOrderClient/src/main/java/br/com/fiap/winery/ApplicationClient2.java
@@ -0,0 +1,24 @@
+package br.com.fiap.winery;
+
+import br.com.fiap.winery.stub_classes.WineStockService;
+import jakarta.xml.ws.Service;
+
+import javax.xml.namespace.QName;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ApplicationClient2 {
+    public static void main(String[] args) throws MalformedURLException {
+        final String WSDL = "http://localhost:8085/WineStockService?wsdl";
+        final String TARGET = "http://winery.fiap.com.br/";
+        final String NAME = "WineStockServiceImplementationService";
+
+        URL url = new URL(WSDL);
+        QName qname = new QName(TARGET, NAME);
+        Service service = Service.create(url, qname);
+
+        WineStockService wineStockService = service.getPort(WineStockService.class);
+        String order = wineStockService.placeOrder("Eduardo", 20);
+        System.out.println(order);
+    }
+}
